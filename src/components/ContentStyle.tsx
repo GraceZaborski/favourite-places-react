d2b300b3aa140143afb17f5af60466712340a8b2
Grace Zaborski <gracezaborski@Graces-Beamery-MacBook-Pro.local>
2021-10-24 00:50:31 +0100

    Dependencies run

--- src/components/ContentStyle.tsx
+++ src/components/ContentStyle.tsx
@@ -1,21 +1,28 @@
 interface ContentStyleProps {
-    title: string;
-    image: string;
-    imageAlt: string;
-    placeName: string;
-    map: string;
-    reason: string;
-  }
-  
-  function ContentStyle(props: ContentStyleProps): JSX.Element {
-    return (
-      <div style= {{textAlign:'center'}}>
-        <h1>{props.title}</h1>
-         <img src={props.image} alt={props.imageAlt} height={'300px'} width={'500px'}/>
-          <h2>{props.placeName} (<a href={props.map}>map</a>)</h2>
-         <h3>{props.reason}</h3>
-      </div>
-    );
-  }
-  
-  export default ContentStyle;
+  title: string;
+  image: string;
+  imageAlt: string;
+  placeName: string;
+  map: string;
+  reason: string;
+}
+
+function ContentStyle(props: ContentStyleProps): JSX.Element {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>{props.title}</h1>
+      <img
+        src={props.image}
+        alt={props.imageAlt}
+        height={"300px"}
+        width={"500px"}
+      />
+      <h2>
+        {props.placeName} (<a href={props.map}>map</a>)
+      </h2>
+      <h3>{props.reason}</h3>
+    </div>
+  );
+}
+
+export default ContentStyle;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,16 @@
-
 function Footer(): JSX.Element {
-    return (
-      <div style={{background:'grey', color: 'white', textAlign:'center'}}>
-        <>
-          <h2>References</h2>
-          <h3>A guide to the Concrete Jungle, 2020, Penguin Books</h3>
-          <h3>Travel Australia, 2015, Jackson and Jackson</h3>
-          <h3>Scenic routes for the travelled Londoner, Parker Publishers, 2017</h3>
-        </>
-      </div>
-    );
-  }
-  
+  return (
+    <div style={{ background: "grey", color: "white", textAlign: "center" }}>
+      <>
+        <h2>References</h2>
+        <h3>A guide to the Concrete Jungle, 2020, Penguin Books</h3>
+        <h3>Travel Australia, 2015, Jackson and Jackson</h3>
+        <h3>
+          Scenic routes for the travelled Londoner, Parker Publishers, 2017
+        </h3>
+      </>
+    </div>
+  );
+}
+
 export default Footer;
