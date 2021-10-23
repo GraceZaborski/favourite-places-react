11a03d2c6ff692471fdd00850d753b7a4db4ba7b
Grace Zaborski <gracezaborski@Graces-Beamery-MacBook-Pro.local>
2021-10-24 00:55:26 +0100

    Re-run prettier

--- src/utils/greet.test.ts
+++ src/utils/greet.test.ts
@@ -3,5 +3,7 @@ import { greet } from "./greet";
 test("greet returns a string, greeting the passed name", () => {
   expect(greet("World")).toBe("Welcome to World's favourite places!");
   expect(greet("Richard")).toBe("Welcome to Richard's favourite places!");
-  expect(greet("Academy Scholars")).toBe("Welcome to Academy Scholars's favourite places!");
+  expect(greet("Academy Scholars")).toBe(
+    "Welcome to Academy Scholars's favourite places!"
+  );
 });
